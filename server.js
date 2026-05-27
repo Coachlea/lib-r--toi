871b4b1152651e54ac74c5b88a98a3a0b24e9018
Coachlea <coach.lea@yahoo.com>
2026-05-27 17:08:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ res.writeHead(200, { “Content-Type”: “text/plain” });
 res.end(“Liber-toi backend actif”);
 });
 
-server.listen(10000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
 console.log(“Serveur lancé”);
 });
